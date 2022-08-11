4da1afcd9260a27f9e250ef23793f3171a10657d
Shahar Yakir <shahar.yakir@gmail.com>
2022-08-11 17:32:04 +0300

    -

--- src/components/add-contract/AddFilesButton.tsx
+++ src/components/add-contract/AddFilesButton.tsx
@@ -3,19 +3,13 @@ import { useCallback, useRef } from "react";
 import { useFileState } from "../../store/useFileState";
 import { useDropzone } from "react-dropzone";
 
-export const onDrop = (acceptedFiles: any) => {
-  console.log(acceptedFiles, "1");
-};
-
 export function AddFilesButton() {
   const { addFiles } = useFileState();
   const inputRef = useRef(null);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+  const { getInputProps } = useDropzone();
 
   const onUploadFiles = async (e: any) => {
-    // @ts-ignore
-    console.log(Array.from(e.target.files).map((f) => f.name));
     await addFiles(Array.from(e.target.files));
   };
 
